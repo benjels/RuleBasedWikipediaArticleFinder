9753a8eff15cda3ec391d017d3713736dbbeed75
Max <maxbrown0101@gmail.com>
2016-01-21 19:57:49 +1300

    added chris' yaml file

--- WikipediaNZArticleFinderPycharmProj/nzwikicrawl.py
+++ WikipediaNZArticleFinderPycharmProj/nzwikicrawl.py
@@ -7,8 +7,6 @@ class Harvester(object):
     # setup the harvester
     def __init__(self, ruleset='rules.yml', verbose=False):
         self.base_url = 'https://en.wikipedia.org/wiki/'
-        # use set rather than list to ensure uniqueness
-        self.topics = set()
         # rules tell the harvester what lists pages to grab
         # and how to evaluate urls on that page
         with open(ruleset, 'r') as stream:
@@ -27,16 +25,18 @@ class Harvester(object):
         return r.content
 
     # extract links from a wikipedia page
-    def extract(self, w_id, rules):
+    def extract(self, w_id, rules): #this receives the url of the list page
         page = self.get(w_id)
         soup = BeautifulSoup(page, 'html.parser')
+        links_from_page = set()
         for scope in rules['scopes']:
             subsection = soup.select(scope)
             for link in subsection:
                 url = link.get('href')
                 topic = self.evaluate_topic_url(url, rules)
                 if topic:
-                    self.topics.add(topic)
+                    links_from_page.add(topic)
+        return links_from_page
         
     # check whether a topic url meets our rules
     def evaluate_topic_url(self, url, rules):
@@ -50,18 +50,22 @@ class Harvester(object):
 
         return url.replace('/wiki/', '')
 
+    #TODO: this should generate a list and return it rather than setting an instance property that gets collected in main. Just a better design
     # work through the ruleset
     def crawl(self):
         seeds = self.ruleset['seeds']
-        for w_id, rules in seeds.items():
-            self.extract(w_id, rules)
+        links_from_root_lists = set()
+        for w_id, rules in seeds.items():#so basically the w_id is each entry in the "seeds" list (just 2 of them at the moment). and rules is everything underneath each w_id. at the moment only "scopes". also at the moment each scopes only has one entry
+           links_from_root_lists = links_from_root_lists.union(self.extract(w_id, rules))
+        #we finished gathering all of the topics, so return them
+        return links_from_root_lists
 
 def main():
     harvester = Harvester()
     harvester.verbose = True
-    harvester.crawl()
     # do something with the resulting topics
-    pp(list(harvester.topics))
+    pp(list(harvester.crawl()))
 
 if __name__ == '__main__':
     main()
+    print("program finished naturally...")
